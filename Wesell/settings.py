de61da234e66e51bf1cff069f20a069ee1e2d7a0
isurya7 <leosuryaof7@gmail.com>
2024-06-30 03:43:42 +0530

    Whitenoise Setting

--- Wesell/settings.py
+++ Wesell/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,6 +88,8 @@ DATABASES = {
         'PORT': env('DATABASE_PORT'),
     }
 }
+
+# postgresql://postgres:JDWQrvNyrfhNicUhfUnbUWJMJSieixee@roundhouse.proxy.rlwy.net:14770/railway
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
